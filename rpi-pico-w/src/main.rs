83f6739951cd4145ff2061b134820ad9a109a017
Aaron Tsui <aaron.tsui@outlook.com>
2022-12-18 22:37:41 +0800

    move wifi init to other file. TODO:     stack.run()?

--- rpi-pico-w/src/main.rs
+++ rpi-pico-w/src/main.rs
@@ -6,15 +6,12 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_net::{Stack, StackResources, Ipv4Address, Ipv4Cidr};
-use embassy_rp::gpio::{Flex, Level, Output};
 use embassy_time::{Duration, Timer};
 use embassy_sync::blocking_mutex::raw::NoopRawMutex;
 use embassy_sync::channel::{Channel};
-use embedded_hal_async::spi::{ExclusiveDevice};
-use heapless::{Vec};
-use static_cell::StaticCell;
+use embassy_net::{Stack};
 use {defmt_rtt as _, panic_probe as _};
+use static_cell::StaticCell;
 
 mod get_temperature;
 mod logger;
@@ -23,89 +20,34 @@ mod tcp;
 mod udp;
 mod v4;
 mod wifi;
-
-macro_rules! singleton {
-    ($val:expr) => {{
-        type T = impl Sized;
-        static STATIC_CELL: StaticCell<T> = StaticCell::new();
-        STATIC_CELL.init_with(move || $val)
-    }};
-}
-
 pub enum Services {
     Temperature,
     DNS,
     NTP,
     Other,
 }
+static CHANNEL: StaticCell<Channel<NoopRawMutex, &'static Stack<cyw43::NetDevice<'static>>, 1>> = StaticCell::new();
 static IN_CHANNEL: StaticCell<Channel<NoopRawMutex, Services, 1>> = StaticCell::new();
 static OUT_CHANNEL: StaticCell<Channel<NoopRawMutex, f32, 1>> = StaticCell::new();
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
+    let wifi_stack = CHANNEL.init(Channel::new());
     let _in = IN_CHANNEL.init(Channel::new());
     let out = OUT_CHANNEL.init(Channel::new());
 
-    // Include the WiFi firmware and Country Locale Matrix (CLM) blobs.
-    // let fw = include_bytes!("../firmware/43439A0.bin");
-    // let clm = include_bytes!("../firmware/43439A0_clm.bin");
-
-    // To make flashing faster for development, you may want to flash the firmwares independently
-    // at hardcoded addresses, instead of baking them into the program with `include_bytes!`:
-    //     probe-rs-cli download 43439A0.bin --format bin --chip RP2040 --base-address 0x10100000
-    //     probe-rs-cli download 43439A0.clm_blob --format bin --chip RP2040 --base-address 0x10140000
-    let fw = unsafe { core::slice::from_raw_parts(0x10100000 as *const u8, 224190) };
-    let clm = unsafe { core::slice::from_raw_parts(0x10140000 as *const u8, 4752) };
-
-    let pwr = Output::new(p.PIN_23, Level::Low);
-    let cs = Output::new(p.PIN_25, Level::High);
-    let clk = Output::new(p.PIN_29, Level::Low);
-    let mut dio = Flex::new(p.PIN_24);
-    dio.set_low();
-    dio.set_as_output();
-
-    let bus = wifi::Spi { clk, dio };
-    let spi = ExclusiveDevice::new(bus, cs);
-
-    let state = singleton!(cyw43::State::new());
-    let (mut control, runner) = cyw43::new(state, pwr, spi, fw).await;
-
-    spawner.spawn(wifi::wifi_task(runner)).unwrap();
-
-    let net_device = control.init(clm).await;
-
-    //control.join_open(env!("WIFI_NETWORK")).await;
-    control.join_wpa2(env!("WIFI_NETWORK"), env!("WIFI_PASSWORD")).await;
-
-    let mut dns_servers = Vec::new();
-    dns_servers.push(Ipv4Address::new(1, 1, 1, 1)); // Cloudflare DNS
-    dns_servers.push(Ipv4Address::new(8, 8, 8, 8)); // Google DNS
-
-    // let config = embassy_net::ConfigStrategy::Dhcp;
-    let config = embassy_net::ConfigStrategy::Static(embassy_net::Config {
-        address: Ipv4Cidr::new(Ipv4Address::new(192, 168, 31, 111), 24),
-        dns_servers,
-        gateway: Some(Ipv4Address::new(192, 168, 31, 1)),
-    });
-
     // Generate random seed
     let seed = 0x0123_4567_89ab_cdef; // chosen by fair dice roll. guarenteed to be random.
+    spawner.spawn(wifi::init(p.PIN_23, p.PIN_25, p.PIN_29, p.PIN_24, seed, wifi_stack.sender())).unwrap();
 
-    // Init network stack
-    let stack = &*singleton!(Stack::new(
-        net_device,
-        config,
-        singleton!(StackResources::<1, 2, 8>::new()),
-        seed
-    ));
-
-    spawner.spawn(v4::net_task(stack)).unwrap();
+    let stack = wifi_stack.recv().await;
 
     // And now we can use it!
     info!("Application initialized.");
+    spawner.spawn(v4::net_task(stack)).unwrap();
 
-    Timer::after(Duration::from_secs(10)).await;
+    // Timer::after(Duration::from_secs(10)).await;
 
     // spawner.spawn(logger::usb_task(p.USB)).unwrap();
     spawner.spawn(tcp::listen_task(stack, _in.sender())).unwrap();
--- rpi-pico-w/src/wifi.rs
+++ rpi-pico-w/src/wifi.rs
@@ -1,16 +1,85 @@
 use core::convert::Infallible;
-use embassy_rp::gpio::{Flex, Output};
+use embassy_rp::Peripheral;
+use embassy_rp::gpio::{Flex, Level, Output};
 use embassy_rp::peripherals::{PIN_23, PIN_24, PIN_25, PIN_29};
 use embedded_hal_1::spi::ErrorType;
-use embedded_hal_async::spi::{SpiBusFlush, SpiBusRead, SpiBusWrite};
-use embedded_hal_async::spi::{ExclusiveDevice};
+use embedded_hal_async::spi::{SpiBusFlush, SpiBusRead, SpiBusWrite, ExclusiveDevice};
+use embassy_net::{Stack, StackResources, Ipv4Address, Ipv4Cidr};
+use embassy_sync::blocking_mutex::raw::NoopRawMutex;
+use embassy_sync::channel::{Receiver, Sender};
+use static_cell::StaticCell;
+use heapless::{Vec};
 
-#[embassy_executor::task]
-pub async fn wifi_task(
-    runner: cyw43::Runner<'static, Output<'static, PIN_23>, ExclusiveDevice<Spi, Output<'static, PIN_25>>>,
-) -> ! {
-    runner.run().await
+macro_rules! singleton {
+    ($val:expr) => {{
+        type T = impl Sized;
+        static STATIC_CELL: StaticCell<T> = StaticCell::new();
+        STATIC_CELL.init_with(move || $val)
+    }};
 }
+#[embassy_executor::task]
+pub async fn init(pwr: PIN_23,cs: PIN_25,clk: PIN_29,dio: PIN_24, seed: u64,
+                    sender: Sender<'static, NoopRawMutex, &'static Stack<cyw43::NetDevice<'static>>, 1>) {
+    // Include the WiFi firmware and Country Locale Matrix (CLM) blobs.
+    // let fw = include_bytes!("../firmware/43439A0.bin");
+    // let clm = include_bytes!("../firmware/43439A0_clm.bin");
+
+    // To make flashing faster for development, you may want to flash the firmwares independently
+    // at hardcoded addresses, instead of baking them into the program with `include_bytes!`:
+    //     probe-rs-cli download 43439A0.bin --format bin --chip RP2040 --base-address 0x10100000
+    //     probe-rs-cli download 43439A0.clm_blob --format bin --chip RP2040 --base-address 0x10140000
+    let fw = unsafe { core::slice::from_raw_parts(0x10100000 as *const u8, 224190) };
+    let clm = unsafe { core::slice::from_raw_parts(0x10140000 as *const u8, 4752) };
+
+    let pwr = Output::new(pwr, Level::Low);
+    let cs = Output::new(cs, Level::High);
+    let clk = Output::new(clk, Level::Low);
+    let mut dio = Flex::new(dio);
+    dio.set_low();
+    dio.set_as_output();
+
+    let bus = Spi { clk, dio };
+    let spi = ExclusiveDevice::new(bus, cs);
+
+    let state = singleton!(cyw43::State::new());
+    let (mut control, runner) = cyw43::new(state, pwr, spi, fw).await;
+
+    // spawner.spawn(wifi_task(runner)).unwrap();
+    runner.run().await;
+
+    let net_device = control.init(clm).await;
+
+    //control.join_open(env!("WIFI_NETWORK")).await;
+    control.join_wpa2(env!("WIFI_NETWORK"), env!("WIFI_PASSWORD")).await;
+
+    let mut dns_servers = Vec::new();
+    dns_servers.push(Ipv4Address::new(1, 1, 1, 1)); // Cloudflare DNS
+    dns_servers.push(Ipv4Address::new(8, 8, 8, 8)); // Google DNS
+
+    // let config = embassy_net::ConfigStrategy::Dhcp;
+    let config = embassy_net::ConfigStrategy::Static(embassy_net::Config {
+        address: Ipv4Cidr::new(Ipv4Address::new(192, 168, 31, 111), 24),
+        dns_servers,
+        gateway: Some(Ipv4Address::new(192, 168, 31, 1)),
+    });
+
+    // Init network stack
+    let stack = &*singleton!(Stack::new(
+        net_device,
+        config,
+        singleton!(StackResources::<1, 2, 8>::new()),
+        seed
+    ));
+
+    sender.send(stack).await;
+}
+
+// #[embassy_executor::task]
+// pub async fn wifi_task(
+//     runner: cyw43::Runner<'static, Output<'static, PIN_23>, ExclusiveDevice<Spi, Output<'static, PIN_25>>>,
+// ) -> ! {
+//     runner.run().await
+// }
 
 pub struct Spi {
     /// SPI clock
